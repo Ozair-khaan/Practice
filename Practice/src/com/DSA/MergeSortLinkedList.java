d1e907f6b74cd8ef197a9394cc6e62192d0ac7f6
Ozair-khaan <ozairkhantech@gmail.com>
2025-06-23 22:31:08 +0530

    DSA ques

---
+++ Practice/src/com/DSA/MergeSortLinkedList.java
@@ -0,0 +1,88 @@
+package com.DSA;
+
+import java.util.Arrays;
+import java.util.List;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class MergeSortLinkedList {
+//    public static void main(String[] args) {
+//        List<Integer> list = Arrays.asList(13, 198, 108, 175, 94, 70, 81, 56);
+//        sortL
+//    }
+
+    public static void main(String[] args) {
+        // Create an unsorted linked list: 4 -> 2 -> 1 -> 3
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(3);
+
+        MergeSortLinkedList sorter = new MergeSortLinkedList();
+        ListNode sortedHead = sorter.sortList(head);
+
+        printList(sortedHead);
+    }
+
+    private static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+
+        return merge(left, right);
+
+    }
+
+    private ListNode getMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (prev != null) {
+            prev.next = null;
+        }
+        return slow;
+    }
+
+    private ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode tail = dummyHead;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+
+            }
+            tail = tail.next;
+        }
+        tail.next = (list1 != null) ? list1 : list2;
+        return dummyHead.next;
+    }
+}
